839a273633697ffdc36c9ef9b5d46fb0ef1aa6a3
sdwjh <58920506+sdwjh@users.noreply.github.com>
2026-04-30 09:14:15 +0800

    Simplify diy-part1 for Lede

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-
-# Use different feed sources that support HTTPS without auth
-echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
-
-# PassWall uses different repo - openwrt-passwall-packages
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall-packages.git' >>feeds.conf.default
-
-# Disable release upload (fork doesn't have permission)
-sed -i 's/UPLOAD_RELEASE: true/UPLOAD_RELEASE: false/' $GITHUB_WORKSPACE/.github/workflows/openwrt-builder.yml
+# Lede has PassWall built-in, no extra feeds needed
+# Just ensure the feeds are updated
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default 2>/dev/null || true
