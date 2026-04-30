fdece0603b1a5379e043e487916b0543bda7dc3c
sdwjh <58920506+sdwjh@users.noreply.github.com>
2026-04-30 09:14:23 +0800

    Add PassWall feeds for Lede

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Lede has PassWall built-in, no extra feeds needed
-# Just ensure the feeds are updated
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default 2>/dev/null || true
+# Add PassWall feed for Lede
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >> feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
